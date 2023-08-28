b6725a76ecd06f7867f6b5bc0069c20e59a092ad
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-08-28 13:24:23 -0400

    views archivo de barril

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from 'react'
 import { MarketplaceProvider } from './context'
-import { Inicio } from './views/inicio/Inicio'
-import { Productos } from './views/productos/Productos'
-import { DetalleProducto } from './views/detalle-producto/DetalleProducto'
-import { IniciarSesion } from './views/iniciar-sesion/IniciarSesion'
-import { RegistrarUsuario } from './views/registrar-usuario/RegistrarUsuario'
-import { RegistrarProducto } from './views/registrar-producto/RegistrarProducto'
-import { Carrito } from './views/carrito/Carrito'
-import { MiPerfil } from './views/mi-perfil/MiPerfil'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NavModel } from './components/navbar/Navbar'
-import { NotFound } from "./views/404/NotFound";
+import {
+  Inicio,
+  Productos,
+  DetalleProducto,
+  IniciarSesion,
+  RegistrarUsuario,
+  RegistrarProducto,
+  Carrito,
+  MiPerfil,
+  NotFound
+} from './views'
 
 function App() {
 
