d71376a519cb312edaefe5afbdb09704df345950
Carlo Aguirre <carlo_aguirre@outlook.cl>
2023-08-29 16:44:12 -0400

    slider and category filters advance

--- src/components/productos/Productos.jsx
+++ src/components/productos/Productos.jsx
@@ -1,77 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import Carousel from 'react-bootstrap/Carousel';
 import './productos.scss'
+import { Aside } from './AsideProductos';
+import { SliderProductos } from './sliderProductos';
+import { useMarketplace } from '../../context';
 
 export const Productos = ({page}) => {
-  const [productos, setProductos] = useState([]);
+  const {pageProductos} = useMarketplace()
   const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    async function fetchProductos() {
-      try {
-        const response = await fetch('src/db/productos.json');
-        const data = await response.json();
-        setProductos(data);
-        console.log(data)
-      } catch (error) {
-        console.error('Error fetching productos:', error);
-      }
-    }
-
-    fetchProductos();
-  }, []);
-
   const handleSelect = (selectedIndex) => {
     setIndex(selectedIndex);
   };
   return (
     <>
-    <div className={page === 'productos' ? 'grid-container__principal-productos' : 'grid-container__principal'}>
-    <aside className="shadow-aside">
-      {
-        (page=== 'productos')? <h4 className='my-4'>FILTROS</h4>
-                             : <h4 className='my-4'>CATEGORIAS</h4>
-      }
-      <div className={page === 'productos' ? 'categorias-productos' : 'categorias'}>
-        <p>TODAS</p>
-        <p><img src="https://res.cloudinary.com/dezwpnks0/image/upload/c_scale,w_50/v1693330540/Nintendo_red_logo.svg_ygqlsr.webp" alt="nintendo_logo" />  NINTENDO</p>
-        <p> <img src="https://res.cloudinary.com/dezwpnks0/image/upload/c_scale,w_50/v1693330539/Playstation_logo_colour.svg_jpnqwn.webp" alt="logo_playstation" /> PLAYSTATION</p>
-        <p><img src="https://res.cloudinary.com/dezwpnks0/image/upload/c_scale,w_50/v1693330539/sega_abtjgg.webp" alt="logo_sega" /> SEGA</p>
-        <p><img src="https://res.cloudinary.com/dezwpnks0/image/upload/c_scale,w_50/v1693330540/atari_logo_by_dhlarson_d5qqh2n-fullview_fjpwjc.webp" alt="atari_logo" /> ATARI</p>
-      </div>
-    </aside>
-    <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
-      {productos.map((producto, productoIndex) => {
-        if (productoIndex % 6 === 0) {
-          const grupoProductos = productos.slice(productoIndex, productoIndex + 6);
-          
-          return (
-            <Carousel.Item key={productoIndex}>
-              <div className="page-counter">
-                <h1 className='text-light'>PRODUCTOS</h1>
-                Página {index + 1} de {Math.round(productos.length/6)}
-              </div>
-              <main>
-                {grupoProductos.map((productoGrupo) => (
-                  <div key={productoGrupo.id} className={`producto-${productoGrupo.id}`}>
-                    <div className="producto-datos shadow">
-                      <img src={`.././src/assets/${productoGrupo.img1}`} style={{ zoom: "13%" }} alt={productoGrupo.nombre} />
-                      <h3>{productoGrupo.nombre}</h3>
-                      <p>${productoGrupo.precio}</p>
-                      <button className="icon-button">
-                        <span className="material-icons-outlined">add_circle</span>
-                      </button>
-                    </div>
-                  </div>
-                ))}
-              </main>
-            </Carousel.Item>
-          );
-        }
-        return null;
-      })}
-    </Carousel>
-  </div>
+    <div className={page === 'productos'? 'grid-container__principal-productos': 'grid-container__principal'}>
+      <Aside page={page}/>
+      <SliderProductos handleSelect={handleSelect} index={index} pageProductos={pageProductos}/>
+    </div>
       </>
   )
   }
--- src/context.jsx
+++ src/context.jsx
@@ -5,10 +5,44 @@ import { createContext, useState, useEffect, useContext } from "react"
  const MarketplaceContext = createContext({})
 
  export const MarketplaceProvider = ({ children }) =>{
+  
+const [productos, setProductos] = useState([]);
+const [categoria, setCategoria] = useState('all')
+const [pageProductos, setPageProductos] = useState([])
+useEffect(() => {
+  async function fetchProductos() {
+    try {
+      const response = await fetch('src/db/productos.json');
+      const data = await response.json();
+      setProductos(data);
+      setPageProductos(data)
+    } catch (error) {
+      console.error('Error en el fetch', error);
+    }
+  }
+  
+  fetchProductos();
+}, []);
 
- 
+
+
+useEffect(() => {
+  const filtrarProductos = ()=>{
+    if(categoria != 'all'){
+      const newProductos = productos.filter(producto=> producto.categoria === categoria)
+      setPageProductos(newProductos)
+    }else if(categoria === 'all'){
+      setPageProductos(productos)
+    }
+  } 
+  filtrarProductos()
+}, [categoria, productos])
+
     const globalState = {
-       
+       productos,
+       pageProductos,
+       setPageProductos,
+       setCategoria
       }
         return (
             <MarketplaceContext.Provider
